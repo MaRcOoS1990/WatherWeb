3c6580ed42069d599ab8028ad107e67797c7e03a
MaRcOoS <leha.chalov@mail.ru>
2024-03-19 17:59:00 +0300

    create end

--- wather/views.py
+++ wather/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
+    appid = '7f17412caf4dc9e351d7071ef4e60421'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"],
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
+
     return render(request, 'wather/index.html')
